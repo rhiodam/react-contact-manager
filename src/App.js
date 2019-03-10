e86025edee6c274c921dc460afef10bda3f64c02
rhiodam <rhioda.m@gmail.com>
2019-03-10 15:28:50 +0700

    Section 6 no 1 done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 // import Contact from "./components/Contact";
 import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
+import About from "./components/pages/About";
 import LikeDislike from "./components/test/LikeDislike";
 import Like from "./components/test/Like";
 import Counter from "./components/test/Counter";
 import logo from "./logo.svg";
 import "./App.css";
 
-import { Provider } from "./context";
+import {Provider} from "./context";
 
 // Bootstrap
 // import "./bootstrap/dist/css/boostrap.min.css";
@@ -19,11 +21,12 @@ import bootstrap from "bootstrap";
 import AddContact from "./components/contacts/AddContact";
 
 class App extends Component {
-  render() {
-    return (
-      <Provider>
-        <div className="App">
-          {/* <header className="App-header">
+    render() {
+        return (
+            <Provider>
+                <Router>
+                    <div className="App">
+                        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -37,20 +40,21 @@ class App extends Component {
             Learn React
           </a>
         </header> */}
+                        {/* sending branding props to header component */}
+                        <Header branding="Contact Manager"/>
 
-          {/* sending branding props to header component */}
-          <Header branding="Contact Manager" />
-          <AddContact/>
-          {/*<LikeDislike/> */}
-          {/*<Like/>*/}
-          {/*<Counter/>*/}
-          <div className="container">
-            <Contacts />
-          </div>
-        </div>
-      </Provider>
-    );
-  }
+                        <div className="container">
+                            <Switch>
+                                <Route exact path="/" component={Contacts}/>
+                                <Route exact path="/about" component={About}/>
+                                <Route exact path="/contact/add" component={AddContact}/>
+                            </Switch>
+                        </div>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
