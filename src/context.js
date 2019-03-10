64a367d2ce3fbf40b0022541f6711a538c52e98e
rhiodam <rhioda.m@gmail.com>
2019-03-10 10:43:45 +0700

    Update context.js

--- src/context.js
+++ src/context.js
@@ -36,30 +36,31 @@ export class Provider extends Component {
                 age: 24,
                 address : "Tebet barat dalam"
             },
+            // {
             {
-                id: 2,
-                name: "Fitya Fikriyah",
-                email: "fifislk@gmail.com",
-                phone: "081266698197",
-                age: 23,
-                address : "Pinang xxx"
-            },
-            {
+                //     id: 2,
+                //     name: "Fitya Fikriyah",
+                //     email: "fifislk@gmail.com",
+                //     phone: "081266698197",
+                //     age: 23,
+                //     address : "Pinang xxx"
+                // },
                 id: 3,
                 name: "Rhioda Muthie",
                 email: "rhioda.muthie@gmail.com",
                 phone: "085271062544",
                 age: 24,
                 address : "Tebet barat dalam"
-            },
-            {
-                id: 4,
-                name: "Fitya Fikriyah",
-                email: "fifislk@gmail.com",
-                phone: "081266698197",
-                age: 23,
-                address : "Pinang xxx"
             }
+            // ,
+            // {
+            //     id: 4,
+            //     name: "Fitya Fikriyah",
+            //     email: "fifislk@gmail.com",
+            //     phone: "081266698197",
+            //     age: 23,
+            //     address : "Pinang xxx"
+            // }
         ],
         dispatch: action => {
             this.setState(state => reducer(state, action));
