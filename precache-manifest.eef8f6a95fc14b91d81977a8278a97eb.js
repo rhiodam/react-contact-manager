f22b2d64f7b1b0bdea6530df029f6764a9a8f498
rhiodam <rhioda.m@gmail.com>
2019-03-28 05:43:32 +0700

    Updates

---
+++ precache-manifest.eef8f6a95fc14b91d81977a8278a97eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "08553d9e7891371615d6",
+    "url": "/contactmanager/static/js/main.08553d9e.chunk.js"
+  },
+  {
+    "revision": "d392e524123883095577",
+    "url": "/contactmanager/static/js/1.d392e524.chunk.js"
+  },
+  {
+    "revision": "08553d9e7891371615d6",
+    "url": "/contactmanager/static/css/main.a155cb7e.chunk.css"
+  },
+  {
+    "revision": "d392e524123883095577",
+    "url": "/contactmanager/static/css/1.28bc90cc.chunk.css"
+  },
+  {
+    "revision": "c6dc154bd4f6ff03c776ad66318e864e",
+    "url": "/contactmanager/index.html"
+  }
+];
