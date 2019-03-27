7f22f19d934f2cdc9766e03136131778b7856882
rhiodam <rhioda.m@gmail.com>
2019-03-28 04:31:47 +0700

    Update LifecycleMethodTest.js

--- src/components/tests/LifecycleMethodTest.js
+++ src/components/tests/LifecycleMethodTest.js
@@ -2,8 +2,22 @@ import React, {Component} from 'react';
 
 class LifecycleMethodTest extends Component {
 
+    state = {
+        title: '',
+        body: ''
+    }
+
     componentDidMount() {
         console.log('LifecycleMethodTest... componentDidMount')
+
+        fetch('https://jsonplaceholder.typicode.com/posts/1')
+            .then(response => response.json())
+            .then(res =>
+                this.setState({
+                    title: res.title,
+                    body: res.body
+                })
+            )
     }
 
     // 1
@@ -20,11 +34,13 @@ class LifecycleMethodTest extends Component {
     }
 
 
-
     render() {
         return (
             <div>
                 <h1>LifecycleMethodTest Component</h1>
+                <h2>Post</h2>
+                <p>{this.state.title}</p>
+                <p>{this.state.body}</p>
             </div>
         );
     }
