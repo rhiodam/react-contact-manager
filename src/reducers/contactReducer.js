0a51fca6a1322365c2525582864f4c4e8ecd0ca4
Rhioda muthie <rhioda.m@gmail.com>
2019-04-07 09:42:07 +0700

    Add contactReducer and types

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,9 +1,49 @@
-const initialState ={
-    test: 'test'
+import {GET_CONTACTS} from '../actions/types';
+
+const initialState = {
+    contacts: [
+        {
+            id: 1,
+            name: "Rhioda Muthie",
+            email: "rhioda.muthie@gmail.com",
+            phone: "085271062544",
+            age: 24,
+            address: "Tebet barat dalam"
+        },
+        {
+            id: 2,
+            name: "test",
+            email: "test@gmail.com",
+            phone: "143423",
+            age: 23,
+            address: "test xxx"
+        },
+        {
+            id: 3,
+            name: "Rhioda Muthie",
+            email: "rhioda.muthie@gmail.com",
+            phone: "085271062544",
+            age: 24,
+            address: "Tebet barat dalam"
+        },
+        {
+            id: 4,
+            name: "test",
+            email: "test@gmail.com",
+            phone: "143423",
+            age: 23,
+            address: "test xxx"
+        }
+    ]
 };
 
-export default function (state = initialState ,action) {
+export default function (state = initialState, action) {
     switch (action.type) {
+
+        case GET_CONTACTS :
+            return {
+                ...state
+            };
 
         default:
             return state;
